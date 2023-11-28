55fbc75624fde31f83c516d52dd91170b8e4a0fd
jhy2639 <jhy2639@naver.com>
2023-11-28 20:45:07 +0900

    useState로 숫자 기록, 리스팅

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [num, setNum] = useState(1);
+
+  const [numList, setNumList] = useState([]);
+
+  function numRecording(){
+    setNumList([...numList, num]); // JS6문법
+
+    setNum(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          하이하이 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='number'>현재숫자 : { num }</div>
+        <button onClick={() => setNum(num + 1)}>숫자 증가</button>
+        <button onClick={() => setNum(num - 1)}>숫자 감소</button>
+        <button onClick={numRecording}>숫자 기록</button>
+        <h1>저장된 숫자</h1>
+        <ul>
+          {numList.map((num) => (
+            <li>{num}</li>
+          ))}
+        </ul>
     </div>
   );
 }
